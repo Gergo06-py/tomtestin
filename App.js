935b0084d1122eb94419f24bd5182c8b86a983cf
Gergo06-py <borbelygergo06@gmail.com>
2024-02-28 09:27:19 +0100

    Dolgozat kész

--- App.js
+++ App.js
@@ -1,20 +1,120 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [weight, setWeight] = useState(0.0);
+	const [height, setHeight] = useState(0);
+	const [result, setResult] = useState(0.0);
+	const [errorMessage, setErrorMessage] = useState("");
+
+	function calculateBMI() {
+		const bmi = weight / (height * height);
+		checkBMI(bmi);
+		setResult(bmi.toFixed(2));
+		setWeight(0);
+		setHeight(0);
+	}
+
+	function checkBMI(bmi) {
+		if (bmi > 25) {
+			setErrorMessage("Túl nagy a testtömegindex!");
+		} else if (bmi < 18) {
+			setErrorMessage("Túl kicsi a testtömegindex!");
+		} else if (bmi > 18 && bmi < 25) {
+			setErrorMessage("");
+		} else {
+			setErrorMessage("A testtömegindexet nem sikerült kiszámítani!");
+		}
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Testtömegindex Számolása</Text>
+			<View>
+				<View style={styles.containerBorder}>
+					<Text style={styles.inputTitle}>Testtömeg (kg)</Text>
+					<TextInput
+						value={weight}
+						onChangeText={setWeight}
+						style={styles.input}
+						inputType="number"
+					/>
+					<Text style={styles.inputTitle}>Testmagasság (méter)</Text>
+					<TextInput
+						value={height}
+						onChangeText={setHeight}
+						style={styles.input}
+						inputType="number"
+					/>
+				</View>
+				<Pressable style={styles.button} onPress={calculateBMI}>
+					<Text style={styles.buttonText}>Számítás</Text>
+				</Pressable>
+				<View style={styles.containerBorder}>
+					<Text style={styles.inputTitle}>Testtömegindex</Text>
+					<TextInput style={styles.input} value={result} editable={false} />
+					<Text style={styles.errorMessage}>{errorMessage}</Text>
+				</View>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "space-evenly",
+	},
+	containerBorder: {
+		alignItems: "center",
+		borderColor: "black",
+		borderWidth: 1,
+		borderRadius: 10,
+		padding: 10,
+		shadowColor: "#aaa",
+		shadowOffset: {
+			width: 4,
+			height: 4,
+		},
+		shadowRadius: 12,
+	},
+	title: {
+		fontSize: 30,
+		textAlign: "center",
+	},
+	inputTitle: {
+		fontSize: 20,
+	},
+	input: {
+		backgroundColor: "#e8e8e8",
+		fontSize: 15,
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		padding: 10,
+		borderRadius: 10,
+		width: 200,
+	},
+	button: {
+		width: "100%",
+		height: 40,
+		backgroundColor: "white",
+		alignSelf: "center",
+		justifyContent: "center",
+		borderColor: "black",
+		borderWidth: 1,
+		borderRadius: 10,
+		marginVertical: 15,
+	},
+	buttonText: {
+		fontSize: 18,
+		textAlign: "center",
+	},
+	errorMessage: {
+		fontSize: 15,
+		color: "red",
+		textAlign: "center",
+	},
 });
